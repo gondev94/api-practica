73028d6926febb5f615a216260e5297931780943
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2025-11-14 16:51:11 +0100

    add notfound  middleware

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use((req, res, next) => {
+    // res.json({message: "soy el middleware"})
+    console.log(req.method);
+    next();
+})
+
 app.get("/", (req, res) => {
     res.send("bienvenidos a nuestra api rest")
 })
 
+
+import notFound from "./src/middlewares/not-found.js"
+app.use(notFound) // esto para cuando hago funcionalidad importo modulo 
 
 const PORT = 3000
 
